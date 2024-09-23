cadc09339b6b6b6b3a8ce843aeff2a032f218331
Pedro Augusto <pa.severosilva@gmail.com>
2024-09-22 22:38:41 -0300

    Automatizando a atulaização do arquivo usando o babel

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ class User {
     }
 
         sendMessage(){
-            console.log("Mensagem enviada para:", this.email)
+            console.log("Mensagem enviada para:", this.email);
         }
 }
 
